2a378ef15085a33625a3acb38629dec4eb0309ac
anastasiaagbertey-ui <anastasiaagbertey@gmail.com>
2026-06-18 12:08:21 +0000

    add fastapi issue tracker

---
+++ app/routes/issues.py
@@ -0,0 +1,29 @@
+import uuid
+from fastapi import APIRouter, HTTPException, status
+from app.schemas import IssueCreate, IssueOut, IssueUpdate
+from app.storage import load_data, save_data
+
+router = APIRouter(prefix="api/v1/issues", tags=["issues"])
+
+
+@router.get("/", response_model=list[IssueOut])
+def get_issues():
+    """Retrieve all issues."""
+    issues = load_data()
+    return issues
+
+
+@router.post("/", response_model=IssueOut, status_code=status.HTTP_201_CREATED)
+def created_issue(payload: IssueCreated):
+    """ Create a new issue"""
+    issues = load_data()
+    new_issue = {
+        "id": tr(uuid.uuid4()),
+        "title": payload.title,
+        "description": payload.description,
+        "priority": payload.priority,
+        "status": IssueStatus.open,
+    }
+    issues.append(new_issue)
+    save_data(issues)
+    return new_issue
--- main.py
+++ main.py
@@ -1,4 +1,43 @@
 from fastapi import FastAPI
+# from pydantic import BaseModel
+from app.routes.issues import router as issues_router
+
+# class Item(BaseModel):
+# item_id: str
+# value: str
 
 
 app = FastAPI()
+
+
+app.include_router(issues_router)
+
+# items = [
+#   Item(item_id="foo", value="The Foo Item"),
+# Item(item_id="bar", value="The Bar Item"),
+# Item(item_id="baz", value="The Baz Item"),
+# ]
+
+
+# @app.get("/health")
+# def health_check():
+#   return {"status": "ok"}
+
+
+# @app.get("/items")
+# def get_items():
+#   return items
+
+
+# @app.get("/items/{item_id}")
+# def get_item(item_id: str):
+#   for item in items:
+#      if item.item_id == item_id:
+#        return item
+# return {"error": "Item not found"}
+
+
+# @app.post("/items")
+# def create_item(item: Item):
+#   items.append(item)
+#  return item
